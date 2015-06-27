9fd2d35730f0fdcde5fccf9ca04061b66c7118a7
silv6928 <tonysilva.ou@gmail.com>
2015-06-27 13:57:25 -0500

    Hash,Add,Wed,Word completed

--- Project 1/Project 1/Source.cpp
+++ Project 1/Project 1/Source.cpp
@@ -4,26 +4,18 @@
 * Project 1
 */
 #include <iostream>
+#include <cctype>
 #include <ctype.h>
 using namespace std;
 
 // Date Token Class
 //---------------------------------------------------------------
+//---------------------------------------------------------------
 class DateToken{
 
 };
-// Year Token Class
-//---------------------------------------------------------------
-class YearToken{
-
-};
-// Web Address Token
-//---------------------------------------------------------------
-class WebAddressToken{
-
-};
-
 //Token Container class
+//---------------------------------------------------------------
 //---------------------------------------------------------------
 class TokenContainer{
 private:
@@ -48,6 +40,7 @@ TokenContainer::~TokenContainer(){
 }
 //Word Token Class
 //---------------------------------------------------------------
+//---------------------------------------------------------------
 class WordToken{
 private:
 	char* WordTokenChar;
@@ -70,10 +63,14 @@ void WordToken::setToken(char* x){
 char* WordToken::getToken(){
 	return WordTokenChar; // might need to return the address on this.
 }
+void WordToken::printToken(){
+	cout << "<" << WordTokenChar << ">";
+}
 WordToken::~WordToken(){
 
 }
 // Hash Tag Class
+//------------------------------------------------------------
 //------------------------------------------------------------
 class HashTagToken{
 public:
@@ -94,7 +91,7 @@ void HashTagToken::setToken(char* x){
 	HashTagChar = x;
 }
 void HashTagToken::printToken(){
-	cout << " <" << HashTagChar << "> ";
+	cout << "<" << HashTagChar << ">";
 }
 char* HashTagToken::getToken(){
 
@@ -104,6 +101,7 @@ HashTagToken::~HashTagToken(){
 	
 }
 // Twitter Address Token
+//---------------------------------------------------------------
 //---------------------------------------------------------------
 class TwitterAddressToken{
 private:
@@ -119,7 +117,7 @@ TwitterAddressToken::TwitterAddressToken(){
 
 }
 void TwitterAddressToken::printToken(){
-	cout << "<" << TwitterAddressChar << "> ";
+	cout << "<" << TwitterAddressChar << ">";
 }
 char* TwitterAddressToken::getToken(){
 
@@ -131,12 +129,74 @@ void TwitterAddressToken::setToken(char* x){
 TwitterAddressToken::~TwitterAddressToken(){
 
 }
+// Web Address Token
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+class WebAddressToken{
+private:
+	char* WebAddressChar;
+public:
+	WebAddressToken();
+	void printToken();
+	char* getToken();
+	void setToken(char* x);
+	virtual ~WebAddressToken();
+};
+WebAddressToken::WebAddressToken(){
+
+}
+void WebAddressToken::printToken(){
+	cout << "<" << WebAddressChar << ">";
+}
+char* WebAddressToken::getToken(){
+
+	return WebAddressChar;
+}
+void WebAddressToken::setToken(char* x){
+	WebAddressChar = x;
+}
+WebAddressToken::~WebAddressToken(){
+
+}
+// Year Token Class
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+class YearToken{
+private: 
+	char* YearTokenChar;
+public:
+	YearToken();
+	void printToken();
+	char* getToken();
+	void setToken(char* x);
+	virtual ~YearToken();
+};
+YearToken::YearToken(){
+
+}
+void YearToken::printToken()
+{
+	cout << "<" << YearTokenChar << ">";
+}
+char* YearToken::getToken()
+{
+	return YearTokenChar;
+}
+void YearToken::setToken(char* x)
+{
+	YearTokenChar = x;
+}
+YearToken::~YearToken(){
+
+}
+//Function to clear pass array
 void emptyString(char* s, int length)
 {
 	for (int i = 0; i < length; i++)
 		s[i] = '\0';
 }
-//------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
 int main(){
 	
 	
@@ -147,11 +207,13 @@ int main(){
 	 // this is to pass what is scanned into the setToken methods // I need to put this as a pointer...
 	
 
-	cin.get(c);
+	
 	while (!cin.eof()){
+		cin.get(c);
 		i = 0;
 		emptyString(pass, 142);
-		if (c == '@'){
+		//Test for Twitter Address
+		if (c == '@'){ 
 			TwitterAddressToken* TwitterAddObj = NULL;
 			TwitterAddObj = new TwitterAddressToken();
 			while ((c != ' ')&&(c != '\'')&&(c != '.')&&(c != '!') && (c != ':')&&(c!= '\n')&&(c!=',')){
@@ -161,29 +223,69 @@ int main(){
 			}
 			(*TwitterAddObj).setToken(pass);
 			(*TwitterAddObj).printToken();
+			cout << c; //printing spaces
 			delete TwitterAddObj;
 		}
-		else if (c == '#') {
+		else if (c == '#') {  //test for HashTags
+			//Hash tag token generated
 			HashTagToken* hashTagobj = NULL;
 			hashTagobj = new HashTagToken();
-			
 			while ((c != ' ') && (c != '\'') && (c != '.') && (c != '!') && (c != ':') && (c != '\n') && (c != ','))
 			{
-				pass[i] = c;   //generating character array to set the Token
+				pass[i] = c;   
 				i++;
 				cin.get(c);
 			}
+			//establish the object and print
 			(*hashTagobj).setToken(pass);
 			(*hashTagobj).printToken();
+			cout << c; //print spaces after token
+			//This is where the token container needs to go.
 			delete hashTagobj;
-		} 
-		else if (c == '\n'){
+		}  // testing for word tokens
+		else if (isalpha(unsigned char(c))){
+			WordToken* WordTokenObj = NULL;
+			WebAddressToken* WebAddObj = NULL;
+			WordTokenObj = new WordToken();
+			while ((c != ' ') && (c != '\'') && (c != ':')&&(c!='"') && (c!='.')&&(c != '!') && (c != '\n') && (c != ',')&&(c!='@')&&(c!='-')){
+
+				pass[i] = c;
+				i++;
+				cin.get(c);
+				//checking if it is a webaddress
+				if ((pass[0] == 'h') && (pass[1] == 't') && (pass[2] == 't')){
+					break;
+				}
+			}
+			if ((pass[0] == 'h') && (pass[1] == 't') && (pass[2] == 't')){
+				WebAddObj = new WebAddressToken();
+				while ((c != ' ') && (c != '\'') && (c != '!') && (c != '\n') && (c != ',') && (c != '@')){
+					pass[i] = c;
+					i++;
+					cin.get(c);
+				}
+				(*WebAddObj).setToken(pass);
+				(*WebAddObj).printToken();
+				cout << c;
+			}
+			if (!(pass[0] == 'h' && pass[1] == 't'&& pass[2] == 't')){
+				(*WordTokenObj).setToken(pass);
+				(*WordTokenObj).printToken();
+				cout << c; //print the character immediately after token
+			}
+			delete WordTokenObj;
+			delete WebAddObj;
+			
+		}
+		else if (isdigit(unsigned char(c))){
+
+
+		}
+		else{
 			cout << c;
 		}
 		
-		cin.get(c);
-		
-		}
+	 }
 		
 	return 0;
 	}
